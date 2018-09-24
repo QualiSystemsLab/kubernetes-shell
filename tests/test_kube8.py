501ab2188c6bc50c061c769c6cdefe41bf986039
doppleware <roni.dover@gmail.com>
2018-09-24 10:06:34 -0700

    initial commit

---
+++ tests/test_kube8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for `Kube8Driver`
+"""
+
+import unittest
+
+from driver import Kube8Driver
+
+
+class TestKube8Driver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
