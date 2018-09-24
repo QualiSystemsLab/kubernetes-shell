267700908b7865eae07e2360d4a24ddc5f650ecc
alexazarh <aazarh@gmail.com>
2018-09-24 17:01:50 -0400

    creates empty 2nd gen shell cloudprovider template

---
+++ kubernetes/src/driver.py
@@ -0,0 +1,197 @@
+from cloudshell.cp.core import DriverRequestParser
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.cp.core.models import DriverResponse
+from cloudshell.shell.core.driver_context import InitCommandContext, AutoLoadCommandContext, ResourceCommandContext, \
+    AutoLoadAttribute, AutoLoadDetails, CancellationContext, ResourceRemoteCommandContext
+
+
+#from data_model import *  # run 'shellfoundry generate' to generate data model classes
+
+class KubernetesDriver (ResourceDriverInterface):
+
+    def __init__(self):
+        """
+        ctor must be without arguments, it is created with reflection at run time
+        """
+        self.request_parser = DriverRequestParser()
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called everytime a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        pass
+
+    # <editor-fold desc="Discovery">
+
+    def get_inventory(self, context):
+        """
+        Discovers the resource structure and attributes.
+        :param AutoLoadCommandContext context: the context the command runs on
+        :return Attribute and sub-resource information for the Shell resource you can return an AutoLoadDetails object
+        :rtype: AutoLoadDetails
+        """
+
+        # run 'shellfoundry generate' in order to create classes that represent your data model
+
+        return AutoLoadDetails([], [])
+
+    # </editor-fold>
+
+    # <editor-fold desc="Mandatory Commands">
+
+    def Deploy(self, context, request=None, cancellation_context=None):
+        """
+        Deploy
+        :param ResourceCommandContext context:
+        :param str request: A JSON string with the list of requested deployment actions
+        :param CancellationContext cancellation_context:
+        :return:
+        :rtype: str
+        """
+
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        # extract DeployApp action
+        deploy_action = single(actions, lambda x: isinstance(x, DeployApp))
+        
+        # if we have multiple supported deployment options use the 'deploymentPath' property 
+        # to decide which deployment option to use. 
+        deployment_name = deploy_action.actionParams.deployment.deploymentPath
+        
+        deploy_result = _my_deploy_method(context, actions, cancellation_context)
+        return DriverResponse(deploy_result).to_driver_response_json()
+        '''
+        pass
+
+    def PowerOn(self, context, ports):
+        """
+        Will power on the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def PowerOff(self, context, ports):
+        """
+        Will power off the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def PowerCycle(self, context, ports, delay):
+        pass
+
+    def DeleteInstance(self, context, ports):
+        """
+        Will delete the compute resource
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        """
+        pass
+
+    def GetVmDetails(self, context, requests, cancellation_context):
+        """
+
+        :param ResourceCommandContext context:
+        :param str requests:
+        :param CancellationContext cancellation_context:
+        :return:
+        """
+        pass
+
+    def remote_refresh_ip(self, context, ports, cancellation_context):
+        """
+        Will update the address of the computer resource on the Deployed App resource in cloudshell
+        :param ResourceRemoteCommandContext context:
+        :param ports:
+        :param CancellationContext cancellation_context:
+        :return:
+        """
+        pass
+
+    # </editor-fold>
+
+
+    ### NOTE: According to the Connectivity Type of your shell, remove the commands that are not
+    ###       relevant from this file and from drivermetadata.xml.
+
+    # <editor-fold desc="Mandatory Commands For L2 Connectivity Type">
+
+    def ApplyConnectivityChanges(self, context, request):
+        """
+        Configures VLANs on multiple ports or port-channels
+        :param ResourceCommandContext context: The context object for the command with resource and reservation info
+        :param str request: A JSON string with the list of requested connectivity changes
+        :return: a json object with the list of connectivity changes which were carried out by the driver
+        :rtype: str
+        """
+        pass
+
+    # </editor-fold> 
+
+    # <editor-fold desc="Mandatory Commands For L3 Connectivity Type">
+
+    def PrepareSandboxInfra(self, context, request, cancellation_context):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :param CancellationContext cancellation_context:
+        :return:
+        :rtype: str
+        """
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        action_results = _my_prepare_connectivity(context, actions, cancellation_context)
+        
+        return DriverResponse(action_results).to_driver_response_json()    
+        '''
+        pass
+
+    def CleanupSandboxInfra(self, context, request):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :return:
+        :rtype: str
+        """
+        '''
+        # parse the json strings into action objects
+        actions = self.request_parser.convert_driver_request_to_actions(request)
+        
+        action_results = _my_cleanup_connectivity(context, actions)
+
+        return DriverResponse(action_results).to_driver_response_json()    
+        '''
+        pass
+
+    # </editor-fold>
+
+    # <editor-fold desc="Optional Commands For L3 Connectivity Type">
+
+    def SetAppSecurityGroups(self, context, request):
+        """
+
+        :param ResourceCommandContext context:
+        :param str request:
+        :return:
+        :rtype: str
+        """
+        pass
+
+    # </editor-fold>
+
+    def cleanup(self):
+        """
+        Destroy the driver session, this function is called everytime a driver instance is destroyed
+        This is a good place to close any open sessions, finish writing to log files, etc.
+        """
+        pass
