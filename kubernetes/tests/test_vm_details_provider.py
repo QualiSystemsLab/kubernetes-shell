d34eca0bb0b75d9ea5eab7d964a9bde3898cf594
alexazarh <aazarh@gmail.com>
2018-10-26 16:07:09 -0400

    exposed attribute to select external service type
    updated vm_details accordingly to external service type
    fixed and added unit tests

--- kubernetes/tests/test_vm_details_provider.py
+++ kubernetes/tests/test_vm_details_provider.py
@@ -32,7 +32,7 @@ class TestVmDetailsProvider(unittest.TestCase):
         self.assertEquals(self._get_vm_prop(result, 'External IP').value, '')
         self.assertEquals(self._get_vm_prop(result, 'External Ports').value, '')
 
-    def test_create_vm_details_from_deployment_and_services(self):
+    def test_create_vm_details_from_deployment_and_services_with_type_load_balancer(self):
         # arrange
         deployment = MagicMock()
         deployment.spec.template.spec.containers = [Mock(image='ubuntu:16.04'), Mock(image='ubuntu:16.04'), Mock(image='ubuntu:14.04')]
@@ -60,6 +60,41 @@ class TestVmDetailsProvider(unittest.TestCase):
         self.assertEquals(self._get_vm_prop(result, 'Internal Ports').value, '8080, 1234, 5678')
         self.assertEquals(self._get_vm_prop(result, 'External IP').value, '')
         self.assertEquals(self._get_vm_prop(result, 'External Ports').value, '81, 82')
+
+    def test_create_vm_details_from_deployment_and_services_with_type_load_nodeport(self):
+        # arrange
+        deployment = MagicMock()
+        deployment.spec.template.spec.containers = [Mock(image='ubuntu:16.04'), Mock(image='ubuntu:16.04'),
+                                                    Mock(image='ubuntu:14.04')]
+
+        internal_service = Mock()
+        internal_service.metadata.labels = {'cloudshell-internal-service': True}
+        internal_service.spec.ports = [Mock(port=8080), Mock(port=1234), Mock(port=5678)]
+
+        external_service = Mock()
+        external_service.metadata.labels = {'cloudshell-external-service': True}
+        port1 = Mock()
+        port2 = Mock()
+        external_service.spec.ports = [port1, port2]
+        external_service.spec.type = 'NodePort'
+
+        # act
+        result = self.vm_details_provider.create_vm_details([internal_service, external_service], deployment)
+
+        # assert
+        self.assertFalse(result.vmNetworkData)
+        self.assertEquals(len(result.vmInstanceData), 7)
+
+        self.assertIn('ubuntu:16.04', self._get_vm_prop(result, 'Image').value)
+        self.assertIn('ubuntu:14.04', self._get_vm_prop(result, 'Image').value)
+        self.assertEquals(self._get_vm_prop(result, 'Replicas').value, deployment.spec.replicas)
+        self.assertEquals(self._get_vm_prop(result, 'Ready Replicas').value, deployment.status.ready_replicas)
+        self.assertEquals(self._get_vm_prop(result, 'Internal IP').value, internal_service.spec.cluster_ip)
+        self.assertEquals(self._get_vm_prop(result, 'Internal Ports').value, '8080, 1234, 5678')
+        self.assertEquals(self._get_vm_prop(result, 'External IP').value, '')
+        self.assertEquals(self._get_vm_prop(result, 'External Ports').value,
+                          "{0}:{1}, {2}:{3}"
+                          .format(port1.port, port1.node_port, port2.port, port2.node_port))
 
     def test_create_vm_details_from_deployment_and_deployed_app(self):
         # arrange
